952aa72715b06168d28161ba01a1fdcf186f51d6
00214884 <jorge.e.gutierrez@aa.com>
2020-07-27 16:09:49 -0500

    All Gatling training material

---
+++ src/test/scala/simulations/AddPauseTime.scala
@@ -0,0 +1,29 @@
+package simulations
+
+import io.gatling.core.Predef._
+import io.gatling.http.Predef._
+import scala.concurrent.duration.DurationInt
+
+class AddPauseTime extends Simulation {
+
+  val httpConf = http.baseUrl("http://localhost:8080/app/")
+    .header("Accept", "application/json")
+
+  val scn = scenario("Video Game DB - 3 Calls")
+    .exec(http("Get all video games - 1st call")
+    .get("videogames"))
+    .pause(5)
+
+    .exec(http("Get specific game - 2nd call")
+    .get("videogames/1"))
+    .pause(1, 20)
+
+    .exec(http("Get all video games - 3rd call")
+    .get("videogames"))
+    .pause(3000.milliseconds)
+
+  setUp(
+    scn.inject(atOnceUsers(1))
+  ).protocols(httpConf)
+
+}
